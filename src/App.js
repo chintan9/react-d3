0c37c9c092299d2721c233868cc8a83a618e671f
Chintan Prajapati <chintan9@live.in>
2016-08-07 02:45:45 -0500

    added chart,js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import chart from 'chart.js'; chart.js
+import Chart from './Chart.js';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Chart/>
       </div>
     );
   }
